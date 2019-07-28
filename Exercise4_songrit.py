920a813141ebe43b535c129a04b2e0c4af04a504
songrit13big <52350511+songrit13big@users.noreply.github.com>
2019-07-28 22:20:00 +0700

    Add files via upload

---
+++ Exercise4_songrit.py
@@ -0,0 +1,9 @@
+b=85
+i=80
+g=75
+a=70
+print("------Your Score------")
+print("Foundation English",":",b)
+print("General Business",":",i)
+print("Introduction to Computer",":",g)
+print("Computer Programming",":",a)
